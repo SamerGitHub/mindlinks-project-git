7cfcd884602d072ca63ab06750d71209178df43c
SarraBenHaddada7 <sarrabenhaddada7@gmail.com>
2020-03-14 11:02:49 +0100

    add vehicule

--- src/Véhicule.java
+++ src/Véhicule.java
@@ -1,4 +1,19 @@
 
 public class Véhicule {
+private String couleur ;
+private int matricule ;
+public String getCouleur() {
+	return couleur;
+}
+public void setCouleur(String couleur) {
+	this.couleur = couleur;
+}
+public int getMatricule() {
+	return matricule;
+}
+public void setMatricule(int matricule) {
+	this.matricule = matricule;
+}
+
 
 }
