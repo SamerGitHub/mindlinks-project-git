f950880169ed892f88afb89a85f652b1636e262c
SarraBenHaddada7 <sarrabenhaddada7@gmail.com>
2020-03-14 11:40:26 +0100

    ajout d'un constructeur

--- src/Voiture.java
+++ src/Voiture.java
@@ -3,6 +3,12 @@ public class Voiture {
 	
   public int nummatricule ;
    public Personne user ;
+public Voiture(int nummatricule, Personne user) {
+	super();
+	this.nummatricule = nummatricule;
+	this.user = user;
+}
+   
    
 
 }
